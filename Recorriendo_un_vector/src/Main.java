5da8b4ac3d9283a3ed15d555875171231b35889d
Jarro22 <jhamil12junio33@gmail.com>
2025-03-21 09:42:04 -0400

    Add files via upload

---
+++ Recorriendo_un_vector/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+       // Ejercicio1 ejercicio1=new Ejercicio1();
+       // ejercicio1.ejecutar();
+     //   Ejercicio2 ejercicio2= new Ejercicio2();
+       // ejercicio2.ejecutar();
+   Ejercicio3 ejercicio3 = new Ejercicio3();
+   ejercicio3.ejecutar();
+
+        }
+    }
